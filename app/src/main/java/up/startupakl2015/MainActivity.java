50aea282286dd1223ca9fde6c669e6d80b8cff12
Justin Tan <justin.njl.tan@gmail.com>
2015-06-13 15:46:08 +1200

    estimote beacon ranging complete

--- app/src/main/java/up/startupakl2015/MainActivity.java
+++ app/src/main/java/up/startupakl2015/MainActivity.java
@@ -1,19 +1,75 @@
 package up.startupakl2015;
 
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.RemoteException;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.estimote.sdk.Beacon;
+import com.estimote.sdk.BeaconManager;
+import com.estimote.sdk.Region;
 import com.estimote.sdk.Utils;
+
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private Beacon beacon;
+    private BeaconManager beaconManager;
+    private Region region;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        region = new Region("regionid", "b9407f30-f5f8-466e-aff9-25556b57fe6d", 63429, 2793);
+        beaconManager = new BeaconManager(this);
+        beaconManager.setRangingListener(new BeaconManager.RangingListener() {
+
+            @Override
+            public void onBeaconsDiscovered(Region region, final List<Beacon> rangedBeacons) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Beacon foundBeacon = null;
+                        for (Beacon beacon : rangedBeacons) {
+                            if (beacon.getMajor() == 63429) {
+                                foundBeacon = beacon;
+                                updateDistance(foundBeacon);
+                            }
+                        }
+                    }
+                });
+            }
+
+        });
+
+    }
+
+    private void updateDistance(Beacon foundBeacon) {
+        TextView text = (TextView) findViewById(R.id.estimote_distance);
+        double distance = Utils.computeAccuracy(foundBeacon);
+        text.setText(String.format("Distance: (%.2fm)", distance));
+
+        if (distance > 10) {
+            try {
+                Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+                Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
+                r.play();
+                onStop();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
@@ -36,5 +92,28 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override
+            public void onServiceReady() {
+                try {
+                    beaconManager.startRanging(region);
+                } catch (RemoteException e) {
+                    Toast.makeText(MainActivity.this, "Cannot start ranging, something terrible happened",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onStop() {
+        beaconManager.disconnect();
+
+        super.onStop();
     }
 }
